6d0194d700014db66274a0e1ff1d94f8a4ce5b99
mararo-ops <danielmararo1@gmail.com>
2023-10-06 14:14:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -20,4 +20,70 @@ const allWagesFor = function () {
 
     return payable
 }
+// Create a single employee record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Convert arrays of employee data into records
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+// Record the time when an employee checks in
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    });
+    return this;
+}
+
+// Record the time when an employee checks out
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    });
+    return this;
+}
+
+// Calculate the hours an employee worked on a given date
+function hoursWorkedOnDate(date) {
+    let inEvent = this.timeInEvents.find(event => event.date === date);
+    let outEvent = this.timeOutEvents.find(event => event.date === date);
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+// Calculate the wages earned by an employee on a given date
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+// Calculate the total wages for an employee
+function allWagesFor() {
+    let eligibleDates = this.timeInEvents.map(event => event.date);
+    return eligibleDates.reduce((memo, date) => memo + wagesEarnedOnDate.call(this, date), 0);
+}
+
+// Search and return an employee by their first name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(emp => emp.firstName === firstName);
+}
+
+// Calculate the total payroll for all employees
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((memo, record) => memo + allWagesFor.call(record), 0);
+}
+
+// Note: The use of 'call' in the functions is to ensure that we use the correct 'this' context, especially when the function is being invoked from another function.
 
